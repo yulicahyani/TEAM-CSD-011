22ec3bdb23f77280b910b70cdfc04273356459c7
yulicahyani <yulicahyani1101@gmail.com>
2021-12-03 15:45:16 +0800

    membuat package untuk login, signup, start dan main

--- Miracle/app/src/main/java/com/csd011/miracle/ui/main/MainActivity.kt
+++ Miracle/app/src/main/java/com/csd011/miracle/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.csd011.miracle
+package com.csd011.miracle.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.csd011.miracle.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
