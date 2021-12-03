681f26fd8b306255bf45fb90c96fb84cc77e46a3
yulicahyani <yulicahyani1101@gmail.com>
2021-12-03 19:10:33 +0800

    membuat fitur deteksi sampah (tampilan layout main dan proses deteksi pada activity main)

--- Miracle/app/src/main/java/com/csd011/miracle/ui/main/MainActivity.kt
+++ Miracle/app/src/main/java/com/csd011/miracle/ui/main/MainActivity.kt
@@ -1,12 +1,272 @@
 package com.csd011.miracle.ui.main
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.*
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.ActionBar
+import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
+import androidx.lifecycle.lifecycleScope
 import com.csd011.miracle.R
+import com.csd011.miracle.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.vision.detector.Detection
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        const val TAG = "TFLite - ODT"
+        const val REQUEST_IMAGE_CAPTURE: Int = 1
+        private const val MAX_FONT_SIZE = 96F
+    }
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var currentImagePath: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.title = "Miracle"
+
+        with(activityMainBinding){
+            captureImageBtn.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE &&
+                resultCode == Activity.RESULT_OK
+        ) {
+            setViewAndDetect(getCapturedImage())
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.captureImageBtn -> {
+                try {
+                    dispatchTakePictureIntent()
+                } catch (e: ActivityNotFoundException) {
+                    Log.e(TAG, e.message.toString())
+                }
+            }
+        }
+    }
+
+    private fun runObjectDetection(bitmap: Bitmap) {
+        val image = TensorImage.fromBitmap(bitmap)
+
+        val options = ObjectDetector.ObjectDetectorOptions.builder()
+                .setMaxResults(5)
+                .setScoreThreshold(0.3f)
+                .build()
+        val detector = ObjectDetector.createFromFileAndOptions(
+                this,
+                "model (3).tflite",
+                options
+        )
+
+        val results = detector.detect(image)
+
+        val resultToDisplay = results.map {
+            val category = it.categories.first()
+            val text = "${category.label}, ${category.score.times(100).toInt()}%"
+
+            DetectionResult(it.boundingBox, text)
+        }
+
+        val imgWithResult = drawDetectionResult(bitmap, resultToDisplay)
+        runOnUiThread {
+            activityMainBinding.imageView.setImageBitmap(imgWithResult)
+        }
+    }
+
+    private fun debugPrint(results : List<Detection>) {
+        for ((i, obj) in results.withIndex()) {
+            val box = obj.boundingBox
+
+            Log.d(TAG, "Detected object: ${i} ")
+            Log.d(TAG, "  boundingBox: (${box.left}, ${box.top}) - (${box.right},${box.bottom})")
+
+            for ((j, category) in obj.categories.withIndex()) {
+                Log.d(TAG, "    Label $j: ${category.label}")
+                val confidence: Int = category.score.times(100).toInt()
+                Log.d(TAG, "    Confidence: ${confidence}%")
+            }
+        }
+    }
+
+    private fun setViewAndDetect(bitmap: Bitmap) {
+        // Display capture image
+        activityMainBinding.imageView.setImageBitmap(bitmap)
+        activityMainBinding.ivPlaceholder.visibility = View.INVISIBLE
+
+        // Run ODT and display result
+        // Note that we run this in the background thread to avoid blocking the app UI because
+        // TFLite object detection is a synchronised process.
+        lifecycleScope.launch(Dispatchers.Default) { runObjectDetection(bitmap) }
+    }
+
+    private fun getCapturedImage(): Bitmap {
+        // Get the dimensions of the View
+        val targetW: Int = activityMainBinding.imageView.width
+        val targetH: Int = activityMainBinding.imageView.height
+
+        val bmOptions = BitmapFactory.Options().apply {
+            // Get the dimensions of the bitmap
+            inJustDecodeBounds = true
+
+            BitmapFactory.decodeFile(currentImagePath, this)
+
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+
+            // Determine how much to scale down the image
+            val scaleFactor: Int = max(1, min(photoW / targetW, photoH / targetH))
+
+            // Decode the image file into a Bitmap sized to fill the View
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inMutable = true
+        }
+        val exifInterface = ExifInterface(currentImagePath)
+        val orientation = exifInterface.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED
+        )
+
+        val bitmap = BitmapFactory.decodeFile(currentImagePath, bmOptions)
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> {
+                rotateImage(bitmap, 90f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_180 -> {
+                rotateImage(bitmap, 180f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_270 -> {
+                rotateImage(bitmap, 270f)
+            }
+            else -> {
+                bitmap
+            }
+        }
+    }
+
+    private fun getSampleImage(drawable: Int): Bitmap {
+        return BitmapFactory.decodeResource(resources, drawable, BitmapFactory.Options().apply {
+            inMutable = true
+        })
+    }
+
+    private fun rotateImage(source: Bitmap, angle: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(angle)
+        return Bitmap.createBitmap(
+                source, 0, 0, source.width, source.height,
+                matrix, true
+        )
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+                "JPEG_${timeStamp}_", /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+        ).apply {
+            currentImagePath = absolutePath
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (e: IOException) {
+                    Log.e(TAG, e.message.toString())
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                            this,
+                            "com.csd011.miracle.fileprovider",
+                            it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    private fun drawDetectionResult(
+            bitmap: Bitmap,
+            detectionResults: List<DetectionResult>
+    ): Bitmap {
+        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(outputBitmap)
+        val pen = Paint()
+        pen.textAlign = Paint.Align.LEFT
+
+        detectionResults.forEach {
+            // draw bounding box
+            pen.color = Color.RED
+            pen.strokeWidth = 8F
+            pen.style = Paint.Style.STROKE
+            val box = it.boundingBox
+            canvas.drawRect(box, pen)
+
+
+            val tagSize = Rect(0, 0, 0, 0)
+
+            // calculate the right font size
+            pen.style = Paint.Style.FILL_AND_STROKE
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 2F
+
+            pen.textSize = MAX_FONT_SIZE
+            pen.getTextBounds(it.text, 0, it.text.length, tagSize)
+            val fontSize: Float = pen.textSize * box.width() / tagSize.width()
+
+            // adjust the font size so texts are inside the bounding box
+            if (fontSize < pen.textSize) pen.textSize = fontSize
+
+            var margin = (box.width() - tagSize.width()) / 2.0F
+            if (margin < 0F) margin = 0F
+            canvas.drawText(
+                    it.text, box.left + margin,
+                    box.top + tagSize.height().times(1F), pen
+            )
+        }
+        return outputBitmap
     }
 }
+
+data class DetectionResult(val boundingBox: RectF, val text: String)
