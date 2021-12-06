8f7071ee87123bfb1b498d60ea01d1eefef0f69d
yulicahyani <yulicahyani1101@gmail.com>
2021-12-07 00:44:19 +0800

    menambah intent di start activity

--- Miracle/app/src/main/java/com/csd011/miracle/ui/start/StartActivity.kt
+++ Miracle/app/src/main/java/com/csd011/miracle/ui/start/StartActivity.kt
@@ -1,12 +1,30 @@
 package com.csd011.miracle.ui.start
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.csd011.miracle.R
+import com.csd011.miracle.databinding.ActivityStartBinding
+import com.csd011.miracle.ui.login.LoginActivity
+import com.csd011.miracle.ui.signup.SignupActivity
 
 class StartActivity : AppCompatActivity() {
+
+    private  lateinit var activityStartBinding: ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        activityStartBinding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(activityStartBinding.root)
+
+        activityStartBinding.loginBtn.setOnClickListener {
+            val intent = Intent(this@StartActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        activityStartBinding.signupBtn.setOnClickListener {
+            val intent = Intent(this@StartActivity, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
