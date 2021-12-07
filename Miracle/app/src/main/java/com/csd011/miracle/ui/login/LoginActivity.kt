e7a01d345bbcce68e5b5bdf269072655b768d421
yulicahyani <yulicahyani1101@gmail.com>
2021-12-07 21:57:11 +0800

    memperbaiki tampilan input (EditText) pada fitur login dan signup

--- Miracle/app/src/main/java/com/csd011/miracle/ui/login/LoginActivity.kt
+++ Miracle/app/src/main/java/com/csd011/miracle/ui/login/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.csd011.miracle.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.csd011.miracle.R
+import com.csd011.miracle.databinding.ActivityLoginBinding
+import com.csd011.miracle.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private  lateinit var activityLoginBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        activityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(activityLoginBinding.root)
+
+        activityLoginBinding.btnSignin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
