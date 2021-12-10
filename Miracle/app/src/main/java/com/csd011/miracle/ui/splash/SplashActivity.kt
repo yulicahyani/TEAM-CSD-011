4cf82a8c4e5b13c867c948c5ad91990d9ae967f4
yulicahyani <yulicahyani1101@gmail.com>
2021-12-10 14:43:21 +0800

    membuat splash activity

--- Miracle/app/src/main/java/com/csd011/miracle/ui/splash/SplashActivity.kt
+++ Miracle/app/src/main/java/com/csd011/miracle/ui/splash/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.csd011.miracle.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.csd011.miracle.R
+import com.csd011.miracle.ui.start.StartActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
